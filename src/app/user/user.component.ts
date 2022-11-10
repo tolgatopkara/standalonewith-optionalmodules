03ae45852da67dd923c288b410f2ae9f9c319ee6
Tolga <tolga.topkara96@gmail.com>
2022-11-10 05:47:44 +0300

    withstandalone

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,13 @@
+import { MatCardModule } from '@angular/material/card';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
+  standalone: true,
+  imports :[MatCardModule],
+  providers: [],
 })
 export class UserComponent implements OnInit {
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app-routing.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent ,{
+  providers : [
+    importProvidersFrom(RouterModule.forRoot(routes)),
+  ],
+})
   .catch(err => console.error(err));
